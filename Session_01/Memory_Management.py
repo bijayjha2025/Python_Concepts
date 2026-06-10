21144f90fb778eed986664f1e77ad32ab25052d4
Bijay Jha <bijayjha12345@gmail.com>
2026-06-10 10:38:12 +0545

    Memory optimization for short integers

--- Session_01/Memory_Management.py
+++ Session_01/Memory_Management.py
@@ -25,3 +25,30 @@ print(id(a), id(b))
 
 if id(a) == id(b):
     print("a and b are referencing the same object in memory.")
+
+
+
+
+
+#Memory Optimization with small integers
+'''
+
+Python applies an internal optimization technique called object interning for small immutable objects (like integers from -5 to 256 and some strings). Instead of creating a new object for each occurrence of the values, Python reuses the same object to save memory.
+
+'''
+
+x = 10
+y = 10
+#Here Python does not create two separate objects for 10. Instead, it creates one object for 10 and both x and y reference that same object in memory.
+print(id(x), id(y))
+
+#Here, both x and y point to the same memory location where the value 10 is stored. This optimization helps reduce memory usage and improve performance when dealing with small integers and certain strings.
+
+
+#But if we change as,
+x= 10
+y = x
+x +=1
+
+if id(x) != id(y):
+    print("x and y are referencing different objects in memory.")
