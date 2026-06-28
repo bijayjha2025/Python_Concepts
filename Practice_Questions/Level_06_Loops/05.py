cc782a01bbbfdb9517ba870e46b9ff639c7cafec
Bijay Jha <bijayjha12345@gmail.com>
2026-06-28 19:25:51 +0545

    loop question practice

---
+++ Practice_Questions/Level_06_Loops/05.py
@@ -0,0 +1,21 @@
+# Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+
+programmingLanguages = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
+for language in programmingLanguages:
+    print(language)
+
+
+# Use for loop to iterate from 0 to 100 and print only even numbers
+
+for i in range(0, 101):
+    if i%2 == 0:
+        print(i)
+
+
+
+# Use for loop to iterate from 0 to 100 and print only odd numbers
+
+for i in range(0, 101):
+    if i%2 != 0:
+        print(i)
+        
