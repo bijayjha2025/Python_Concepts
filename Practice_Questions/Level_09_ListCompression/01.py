3eee7a7bd0a020c0fc7709e5dca82031172581a0
Bijay Jha <bijayjha12345@gmail.com>
2026-07-03 20:26:14 +0545

    list compression questions

---
+++ Practice_Questions/Level_09_ListCompression/01.py
@@ -0,0 +1,37 @@
+# Filter only negative and zero in the list using list comprehension: numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+negativeAndZero = [num for num in numbers if num <= 0]
+print(negativeAndZero)
+
+
+# Flatten the following list of lists of lists to a one dimensional list : list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+listOfLists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattenedList = [num for sublist in listOfLists for num in sublist]
+print(flattenedList)
+
+'''
+Using list comprehension create the following list of tuples:
+[(0, 1, 0, 0, 0, 0, 0),
+(1, 1, 1, 1, 1, 1, 1),
+(2, 1, 2, 4, 8, 16, 32),
+(3, 1, 3, 9, 27, 81, 243),
+(4, 1, 4, 16, 64, 256, 1024),
+(5, 1, 5, 25, 125, 625, 3125),
+(6, 1, 6, 36, 216, 1296, 7776),
+(7, 1, 7, 49, 343, 2401, 16807),
+(8, 1, 8, 64, 512, 4096, 32768),
+(9, 1, 9, 81, 729, 6561, 59049),
+(10, 1, 10, 100, 1000, 10000, 100000)]
+'''
+
+list = [(i, 1, i, i**2, i**3, i**4, i**5) for i in range(11)]
+print(list)
+
+# Flatten the following list to a new list: countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+
+
+countries = ([[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]])
+flattenedCountries = [country for sublist in countries for country in sublist]
+print(flattenedCountries)
