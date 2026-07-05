6bb8fd16fb796c3e207d5439fee39126483610ba
Bijay Jha <bijayjha12345@gmail.com>
2026-07-05 18:01:43 +0545

    continued

--- Session_06/decorators.py
+++ Session_06/decorators.py
@@ -29,3 +29,43 @@ def upperCaseDecorator(function):
 @upperCaseDecorator
 def greeting():
     return "Welcome to Python Decorators!"
+
+
+
+# Applying multiple decorators to a single function
+def upperCaseDecorator(function):
+    def wrapper():
+        func = function()
+        return func.upper()
+    return wrapper
+
+def splitStringDecorator(function):
+    def wrapper():
+        func = function()
+        return ' '.join(func.split())
+    return wrapper
+
+@splitStringDecorator
+@upperCaseDecorator
+
+def greeting():
+    return "Welcome to Python's Multiple Decorators!"
+
+print(greeting())
+
+#Decorators will execute from bottom to top, so in this case, the upperCaseDecorator will be applied first, followed by the splitStringDecorator. The final output will be the result of both decorators applied to the greeting function.
+
+
+# Accepting parameters in decorators
+def decoratorwithParameter(function):
+    def wrapper(parameter):
+        print(f"Decorator received parameter: {parameter}")
+        return function(parameter)
+    
+    return wrapper
+
+@decoratorwithParameter
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Bijay"))
